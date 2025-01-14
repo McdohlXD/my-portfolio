fe9a4140f8403ffd13c4d0d89f8f83e40a7660cf
Mcdohl <Marklv2724@gmail.com>
2025-01-14 17:21:40 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div className="flex justify-center items-center h-screen flex-col">
+      <h1 className="text-5xl text-blue-700">Hello,</h1>
+      <p>Welcome to my portfolio</p>
+    </div>
+  );
+}
