e410bd1172cfa2d5aaa1c9cdd2a31d8e163be8ca
Mcdohl <Marklv2724@gmail.com>
2025-02-17 19:23:39 +0800

    remove 404.html and update others

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import path from 'node:path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/my-portfolio/',
+  base: '/',
   resolve: {
     alias: {
       '@': path.resolve(process.cwd(), './src'),
